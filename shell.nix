cd783b7933fd88f881381d5a4a9c037006a12e42
Mikhail Klementev <blame@dumpstack.io>
2019-08-28 01:15:31 +0000

    Make shell.nix really reproducible

--- shell.nix
+++ shell.nix
@@ -3,9 +3,17 @@
 pkgs.mkShell {
   name = "free-as-in-freedom-v2-ru";
 
+  FONTCONFIG_FILE = pkgs.makeFontsConf {
+    fontDirectories = [ pkgs.liberation_ttf ];
+  };
+
   buildInputs = with pkgs; [
     git utillinux gnumake
     texlive.combined.scheme-full rubber
     pandoc
   ];
+
+  shellHook = ''
+    export LANG=C.UTF-8
+  '';
 }
